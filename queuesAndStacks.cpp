7467575111f6a08380a18bd8a5818c79b41b4399
BoredDany <141600976+BoredDany@users.noreply.github.com>
2023-08-22 16:57:16 -0500

    Update queuesAndStacks.cpp

--- queuesAndStacks.cpp
+++ queuesAndStacks.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
-#include<queue>
 #include<stack>
 #include<list>
 #include<fstream>
 #include<string>
 #include<sstream>
 #include<cstring>
+#include <cctype>
 #include "Directorio.h"
+#include "Resultado.h"
 using namespace std;
 
 int contar (std::string file_name){
@@ -37,7 +38,15 @@ int contarEspacios(std::string line){
     return espacios;
 }
 
-std::stack<std::list<char *>> leerArvivo(std::string file_name, std::string& substring, int& num_rows){
+quitarSignos(std::string& line){
+    for(int i = 0 ; i < line.length() ; i++){
+        if (ispunct(line[i])) {
+                line[i] = ' ';
+    }
+    }
+}
+
+std::stack<std::list<char *>> leerArchivo(std::string file_name, std::string& substring, int& num_rows){
 
     std::ifstream inputF(file_name);
     std::string line, word;
@@ -45,56 +54,57 @@ std::stack<std::list<char *>> leerArvivo(std::string file_name, std::string& sub
     std::stack<std::list<char *>> stackPalabras;
     if(inputF.is_open()){
         for(int j = 0 ; j < sizeF-1 ; j++){
-
             getline(inputF, line,'\n');
             num_rows = stoi(line);
-            cout<<num_rows<<endl;
-
             getline(inputF, line,'\n');
             substring = line;
-            cout<<substring<<endl;
-
             for(int i = 0 ; i < num_rows-1 ; i++){
                 j++;
                 getline(inputF, line);
                 std::list<char *> palabras;
                 int espacios = contarEspacios(line);
                 stringstream ss(line);
-
                 for(int k = 0 ; k < espacios+1 ; k++){
                     getline(ss,word,' ');
-                    palabras.push_back(strcpy(new char[word.length() + 1], word.c_str()));
+                    std::string cleanWord;
+                    for(char c: word){
+                        if(isalpha(c)){
+                            cleanWord+=c;
+                        }
+                    }
+                    if(!cleanWord.empty()){
+                        palabras.push_back(strcpy(new char[cleanWord.length() + 1], cleanWord.c_str()));
+                    }
                 }
                 stackPalabras.push(palabras);
             }
         }
-
-        /*int i = 0;
-        cout<<"STACK--------"<<endl;
-        while(!stackPalabras.empty()){
-        cout<<"Stack "<<i+1<<endl;
-        std::list<char*> actualList = stackPalabras.top();
-        stackPalabras.pop();
-        for (char* s : actualList) {
-            cout << s << endl;
-        }
-        i++;
-        cout<<"-------------------"<<endl;
-        }*/
-
     }else{
         cout<<"No se leyo"<<endl;
     }
     inputF.close();
     return stackPalabras;
-};
+}
 
-void comienzoSubString(Directorio& dic){
+char* SubcadenaInvertida(char * inv){
+    int longitud = std::strlen(inv);
+    for (int i = 0, j = longitud - 1; i < j; i++, j--) {
+        char temp = inv[i];
+        inv[i] = inv[j];
+        inv[j] = temp;
+    }
+    return inv;
+}
+
+void buscar(Directorio& dic, std::list<Resultado>& comienza, std::list<Resultado>& contiene, std::list<Resultado>& contieneInv){
+
     int cont = 0;
-    int contP = 0;
+    char * invC = new char[strlen(dic.get_substring())+1];
+    strcpy(invC, dic.get_substring());
+    SubcadenaInvertida(invC);
+
     std::stack<std::list<char*>> duplicadoPila = dic.getWords();
     std::list<std::list<char*>> duplicadoLista;
-    std::cout<<"Palabras que inician con "<<dic.get_substring()<<std::endl;
     while(!duplicadoPila.empty()){
         duplicadoLista.push_front(duplicadoPila.top());
         duplicadoPila.pop();
@@ -107,40 +117,24 @@ void comienzoSubString(Directorio& dic){
         std::list<char*>::iterator it2daDimension = itDuplicadoLista->begin();
         for(;it2daDimension!=itDuplicadoLista->end();it2daDimension++){
             if(strncmp(*it2daDimension,dic.get_substring(),strlen(dic.get_substring()))==0){
-                std::cout<<"Linea "<<cont<<": "<<*it2daDimension<<std::endl;
-                contP++;
+                Resultado r(cont, *it2daDimension);
+                comienza.push_back(r);
+            }
+            if(std::strstr(*it2daDimension, dic.get_substring()) != nullptr){
+                Resultado r(cont, *it2daDimension);
+                contiene.push_back(r);
+            }
+            if(std::strstr(*it2daDimension, invC) != nullptr){
+                Resultado r(cont, *it2daDimension);
+                contieneInv.push_back(r);
             }
         }
     }
-    std::cout<<"Cantidad de palabras: "<<contP<<std::endl;
-}
-
-void contieneSubcadena(const std::string& substring, int num_rows, std::stack<std::list<char*>>& wordStack) {
-    setlocale(LC_ALL, "Spanish");
-    cout << "\nCONTIENE SUBCADENA: " << endl;
-    cout << "num_rows: " << num_rows << endl;
-    cout << "substring: " << substring << endl;
-
-    int count = 0;
-    int lineNum = 1;
-
-    while (!wordStack.empty()) {
-        std::list<char*> listaActual = wordStack.top();
-        wordStack.pop();
-
-        for (char* word : listaActual) {
-            if (strstr(word, substring.c_str()) != NULL) {
-                count++;
-                cout << "Palabra: " << word << " en la linea " << lineNum << endl;
-            }
-        }
-        lineNum++;
-    }
-
-    cout << "Total de palabras que contienen la subcadena: " << count<<endl;
+    delete[] invC;
 }
 
 int main(){
+
     string file_name;
     cout<<"Ingrese nombre del archivo: ";
     cin>>file_name;
@@ -148,37 +142,31 @@ int main(){
     std::string substring;
     std::stack<std::list<char *>> stackP;
 
-    stackP = leerArvivo(file_name, substring, filas);
-
+    stackP = leerArchivo(file_name, substring, filas);
     Directorio d (filas, &substring[0]);
     d.setStack(stackP);
 
-    cout<<"filas: "<<d.getNum_rows()<<endl;
-    cout<<"substr: "<<d.get_substring()<<endl;
+    cout<<"\nRenglones en el archivo: "<<d.getNum_rows()<<endl;
+    cout<<"Subcadena a buscar: "<<d.get_substring()<<endl<<endl;
 
-    Directorio dic = d;
-    
-    std::stack<std::list<char*>>& wordStack = d.getWords();
-    std::stack<std::list<char*>> copyStack = wordStack; 
+    std::list<Resultado> comienza, contiene, contieneInv;
+    std::list<Resultado>::iterator itResultado = comienza.begin();
+    buscar(d, comienza, contiene, contieneInv);
 
-    int i = 0;
-    while(!wordStack.empty()) {
-        cout << "Stack " << i+1 << endl;
-        std::list<char*> actualList = wordStack.top();
-        wordStack.pop();
-        for (char* s : actualList) {
-            cout << s << endl;
-        }
-        i++;
-        cout << "-------------------" << endl;
+    cout<<"Hay "<<comienza.size()<<" palabras que comienzan por: "<<d.get_substring()<<endl;
+    for(itResultado = comienza.begin() ; itResultado != comienza.end() ; itResultado++){
+        cout<<"Linea "<<itResultado->getLinea()<<": "<<itResultado->getWord()<<endl;
     }
 
-    std::string localSubstring = std::string(d.get_substring());
-    int localNumRows = d.getNum_rows();
-    
-    comienzoSubString(dic);
-    contieneSubcadena(localSubstring, localNumRows, copyStack);
-    return 0;
+    cout<<"\nHay "<<contiene.size()<<" palabras que contienen: "<<d.get_substring()<<endl;
+    for(itResultado = contiene.begin() ; itResultado != contiene.end() ; itResultado++){
+        cout<<"Linea "<<itResultado->getLinea()<<": "<<itResultado->getWord()<<endl;
+    }
+
+    cout<<"\nHay "<<contieneInv.size()<<" palabras que contienen: "<<SubcadenaInvertida(d.get_substring())<<endl;
+    for(itResultado = contieneInv.begin() ; itResultado != contieneInv.end() ; itResultado++){
+        cout<<"Linea "<<itResultado->getLinea()<<": "<<itResultado->getWord()<<endl;
+    }
 
 }
-//g++ -std=c++11 -o testS Directorio.cxx queuesAndStacks.cpp
+//g++ -std=c++11 -o testS Directorio.cxx Resultado.cxx queuesAndStacks.cpp
